a41cf5d4d8add1f131525c39e072d66e569e2e78
Joao Batista <jpcbatista.2004@gmail.com>
2023-11-06 12:07:08 -0300

    📈 Training KNN models

--- Teste.R
+++ Teste.R
@@ -1,5 +1,41 @@
-read_csv = read.csv("./Trabalho.csv", header=TRUE, sep=",")
+#Leitura dos dados
+data <- read.csv("./Trabalho.csv", sep=",", header=TRUE)
 
-print(read_csv)
+#Definicao da coluna Y
+row_labels = data[,2]
 
-#Necessário normalizar os dados de teste usando os dados de treino (média e desvio padrão)
+#Normalizacao dos parametros X1, X2, X3 e X4
+data[,3:6] <- scale(data[,3:6])
+
+#Definindo a seed
+set.seed(123)
+
+#Separando os dados em conjuntos de treino e de teste
+size <- floor(0.7 * nrow(data))
+
+train_ind <- sample(seq_len(nrow(data)), size = size)
+
+train_labels <- data[train_ind, 2]
+test_labels <- row_labels[-train_ind]
+
+data_train<- data[train_ind, 3:6]
+data_test <- data[-train_ind, 3:6]
+
+#Utilizando o método KNN
+library(class)
+predictions1 <- knn(train = data_train,
+                   test = data_test,
+                   cl = train_labels,
+                   k = 1)
+
+predictions3 <- knn(train = data_train,
+                   test = data_test,
+                   cl = train_labels,
+                   k = 3)
+
+predictions5 <- knn(train = data_train,
+                   test = data_test,
+                   cl = train_labels,
+                   k = 5)
+
+print(predictions5)
